d1a50511e8374d54c3ad8a45f850ce0a65da45f8
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-06-19 00:05:01 +0800

    feat: :sparkles: add script to change navbar background on scroll

---
+++ src/scripts/index.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('header');
+  const registerBtn = document.getElementById('register');
+  if (window.scrollY > 0) {
+    header.classList.remove('bg-transparent');
+    header.classList.remove('h-40');
+    header.classList.add('bg-maastrichtBlue');
+    header.classList.add('h-24');
+    registerBtn.classList.remove('bg-white');
+    registerBtn.classList.add('bg-princetonOrange');
+    registerBtn.classList.remove('text-princetonOrange');
+    registerBtn.classList.add('text-white');
+  } else {
+    header.classList.remove('bg-maastrichtBlue');
+    header.classList.remove('h-24');
+    header.classList.add('bg-transparent');
+    header.classList.add('h-40');
+    registerBtn.classList.add('bg-white');
+    registerBtn.classList.remove('bg-princetonOrange');
+    registerBtn.classList.add('text-princetonOrange');
+    registerBtn.classList.remove('text-white');
+  }
+});
