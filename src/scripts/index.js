23f6d0f87889ec5563bdb59c1d967ef06822b10d
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-06-19 23:09:44 +0800

    feat: :sparkles: add new function to index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function () {
+const scrollHandler = () => {
   const header = document.getElementById('header');
   const registerBtn = document.getElementById('register');
   if (window.scrollY > 0) {
@@ -20,4 +20,52 @@ window.addEventListener('scroll', function () {
     registerBtn.classList.add('text-princetonOrange');
     registerBtn.classList.remove('text-white');
   }
+}
+
+const toggleCardSize = (card) => {
+  const linkContainer = card.querySelector('.link-container');
+
+  if (card.classList.contains('w-161.2')) {
+    card.classList.remove('w-161.2');
+    card.classList.add('w-92.7');
+    linkContainer.classList.add('hidden');
+  } else if (card.classList.contains('w-92.7')) {
+    card.classList.remove('w-92.7');
+    card.classList.add('w-161.2');
+    linkContainer.classList.remove('hidden');
+  }
+}
+
+const faqCardToogleHandler = () => {
+  const faqCards = document.querySelectorAll('.faq-card');
+
+  faqCards.forEach((card) => {
+    card.addEventListener('click', () => {
+      const paragraph = card.querySelector('p');
+      const isHidden = paragraph.classList.contains('hidden');
+
+      document
+        .querySelectorAll('.faq-card p')
+        .forEach((p) => p.classList.add('hidden'));
+
+      if (isHidden) {
+        paragraph.classList.remove('hidden');
+      }
+    });
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  faqCardToogleHandler();
+  const cards = document.querySelectorAll('.toogle-card');
+
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      toggleCardSize(card);
+    });
+  });
+});
+
+window.addEventListener('scroll', function () {
+  scrollHandler();
 });
