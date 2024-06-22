7dd6ac94e4bc4246829f0140067f4d9704dd3775
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-06-22 19:15:11 +0800

    refactor: :art: import components

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,14 @@
 import '../styles/output.css';
+import './components/components'
 import scrollHandler from './handlers/scroll-handler-navbar';
 import faqCardToogleHandler from './handlers/faq-card-handler';
 import { setupCardToggleListeners } from './handlers/toggle-card-size-handler';
 
+
 document.addEventListener('DOMContentLoaded', () => {
   scrollHandler();
   faqCardToogleHandler();
-  setupCardToggleListeners()
+  setupCardToggleListeners();
 });
 
 
