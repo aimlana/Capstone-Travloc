6815bc510640a5f81b86b8448de0bbe0a56e99c1
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-06-23 01:11:13 +0800

    feat: :art: finishing landing page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,6 @@ import scrollHandler from './handlers/scroll-handler-navbar';
 import faqCardToogleHandler from './handlers/faq-card-handler';
 import { setupCardToggleListeners } from './handlers/toggle-card-size-handler';
 
-
 document.addEventListener('DOMContentLoaded', () => {
   scrollHandler();
   faqCardToogleHandler();
