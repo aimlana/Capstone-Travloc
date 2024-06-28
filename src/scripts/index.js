c0a4741f3a80a4cd2ccb4acb31301a4331ae9cff
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-06-28 21:23:06 +0800

    chore: :art: change aos function to global DOMContentLoad

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,7 @@ import faqCardToogleHandler from './handlers/faq-card-handler';
 import { setupCardToggleListeners } from './handlers/toggle-card-size-handler';
 
 document.addEventListener('DOMContentLoaded', () => {
+  AOS.init();
   const currentPath = window.location.pathname;
 
   // Periksa apakah halaman adalah index.html atau halaman root
@@ -12,6 +13,5 @@ document.addEventListener('DOMContentLoaded', () => {
     scrollHandler();
     faqCardToogleHandler();
     setupCardToggleListeners();
-    AOS.init();
-  }
+  } 
 });
