782770759c9028d63221b68bc5076b42b4ed1439
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-07-18 21:32:41 +0800

    feat: :sparkles: using umkmDataVIew func at usaha-lokal path

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import './components/components'
 import scrollHandler from './handlers/scroll-handler-navbar';
 import faqCardToogleHandler from './handlers/faq-card-handler';
 import { setupCardToggleListeners } from './handlers/toggle-card-size-handler';
+import umkmDataView from './views/usaha-lokal-data-view';
 
 document.addEventListener('DOMContentLoaded', () => {
   AOS.init();
@@ -13,5 +14,9 @@ document.addEventListener('DOMContentLoaded', () => {
     scrollHandler();
     faqCardToogleHandler();
     setupCardToggleListeners();
-  } 
+  }
+
+  if (currentPath.endsWith('usaha-lokal.html')) {
+    umkmDataView();
+  }
 });
